10beb1cbeb90ba1d6fedf50a29d5617ef20a2349
MohamadCode <m-munla2004@hotmail.com>
2023-12-21 19:31:40 +0300

    local storage value stored

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-let myLeads = ["www.awesomelead.com"]
-myLeads.push("www.google.com")
-myLeads = JSON.stringify(myLeads)
-console.log(myLeads)
+let myLeads = []
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-localStorage.clear()
-
-function renderLeads() {
-let listItems =  " "  
-
-  for (let index = 0; index < myLeads.length; index++) {
-  listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[index]}'>
-                    ${myLeads[index]}
-                </a>
-            </li>
-        `  }
-  ulEl.innerHTML = listItems
-  }
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
-  renderLeads()
   inputEl.value = ""
-})
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  renderLeads()
+
+  console.log( localStorage.getItem("myLeads") )
+}
+)
+
+function renderLeads() {
+  let listItems =  " "  
+  
+    for (let index = 0; index < myLeads.length; index++) {
+    listItems += `
+              <li>
+                  <a target='_blank' href='${myLeads[index]}'>
+                      ${myLeads[index]}
+                  </a>
+              </li>
+          `  }
+    ulEl.innerHTML = listItems
+    }
 
 
 
