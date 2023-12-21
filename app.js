2b00bcb0021cf6033f4ce329bdb2c42a04f3e5e5
MohamadCode <m-munla2004@hotmail.com>
2023-12-21 19:23:09 +0300

    stringifying the array

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com"]
+myLeads.push("www.google.com")
+myLeads = JSON.stringify(myLeads)
+console.log(myLeads)
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
