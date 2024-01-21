b650c85550d146d6040a1fd750c836df01bc26b1
MohamadCode <m-munla2004@hotmail.com>
2024-01-21 10:52:36 +0300

    tabBtn works

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const tabs = [
 ]
 
 tabBtn.addEventListener('click', function(){ 
+  myLeads.push(tabs[0].url)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
   console.log(tabs[0].url)
 
 })
